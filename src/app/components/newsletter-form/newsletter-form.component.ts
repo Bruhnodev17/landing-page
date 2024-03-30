cf1aa51adc7def7ef0d3a448ac30b802ccab83fc
Manoel Bruno <manoelbrunop17@gmail.com>
2024-03-29 21:43:01 -0300

    "<feat>:<siginal>"

--- src/app/components/newsletter-form/newsletter-form.component.ts
+++ src/app/components/newsletter-form/newsletter-form.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { BtnPrimaryComponent } from '../btn-primary/btn-primary.component';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { NewsletterService } from '../../services/newsletter.service';
@@ -15,6 +15,7 @@ import { NewsletterService } from '../../services/newsletter.service';
 })
 export class NewsletterFormComponent {
   newsletterForm!: FormGroup;
+  loading = signal(false);
 
   constructor(private service: NewsletterService) {
     this.newsletterForm = new FormGroup({
@@ -24,10 +25,12 @@ export class NewsletterFormComponent {
   }
 
   onSubmit(){
+    this.loading.set(true)
     if(this.newsletterForm.valid){
       this.service.sendData(this.newsletterForm.value.name, this.newsletterForm.value.email).subscribe({
         next: () => {
           this.newsletterForm.reset
+          this.loading.set(false)
         }
       })
     }
